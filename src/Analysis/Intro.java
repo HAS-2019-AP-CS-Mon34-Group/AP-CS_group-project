43c115a7fd43ba44cb6d84e0d25384da2931b63c
CHEMICALS1234 <ysmsmart1@gmail.com>
2019-11-12 10:58:48 +0900

    wow!

--- src/Analysis/Intro.java
+++ src/Analysis/Intro.java
@@ -1,34 +1,64 @@
 package Analysis;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Intro {
-    public static void main(String[] args) {
+
+    public static ArrayList<String> lineParsing(File file){
         try{
-            File file = new File("C:\\Users\\user\\Desktop\\Code\\Java\\KakaotalkforHAS\\src\\Analysis\\test.txt");
+
 
             FileReader filereader = new FileReader(file);
             //입력 버퍼 생성
             BufferedReader bufReader = new BufferedReader(filereader);
 
-            int iter = 1;
+            ArrayList<String> sent = new ArrayList<>();
 
-            String line;
+            String line="";
 
-            line = bufReader.readLine();
-//            while((line = bufReader.readLine()) != null){
-            System.out.println(line);
+            while(true){
+                line = bufReader.readLine();
+                if(line == null){
+                    break;
+                }
+                if(line.equals("")){
+                    continue;
+                }
+                sent.add(line);
+
+            }
+
+//            for(String s : sent){
+//                System.out.println(s);
 //            }
-            //.readLine()은 끝에 개행문자를 읽지 않는다.
+
             bufReader.close();
+            return sent;
+
+
         }catch (FileNotFoundException e) {
             // TODO: handle exception
+            return null;
         }catch(IOException e) {
             System.out.println(e);
+            return null;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\user\\Documents\\GitHub\\AP-CS_group-project\\src\\Analysis\\test.txt");
+
+        ArrayList<String> sentence = lineParsing(file);
+        for(String s : sentence){
+            System.out.println(s);
         }
     }
 }
