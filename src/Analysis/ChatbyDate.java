30419d667e5e0c98e66cf3d76cbcc15daefefdef
kodh49 <milleniumkodh49@gmail.com>
2019-11-14 12:18:02 +0900

    Recreate chat by date

---
+++ src/Analysis/ChatbyDate.java
@@ -0,0 +1,40 @@
+package Analysis;
+
+import java.lang.reflect.Array;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ChatbyDate {
+
+    public static int[] ScannerDate() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("검색할 날짜를 입력해 주십시오. Ex) 2019.04.29");
+        String input = sc.nextLine();
+        sc.close();
+        String[] ScrDate0 = input.split("\\.");
+        int[] ScrDate = new int[ScrDate0.length];
+        for (int i=0; i<ScrDate.length; i++) {
+            ScrDate[i] = Integer.parseInt(ScrDate0[i]);
+        }
+        return ScrDate;
+    }
+
+    public int ChatByDate(ArrayList<String> DataList, int[] ScrDate) {
+        String[] DateArr = new String[DataList.size()];
+        SplitData.GetDate(DataList);
+        DateArr = SplitData.Date.toArray(DateArr);
+        int count = 0;
+        for (String s : DateArr) {
+            ZonedDateTime DateTime = TimeFormat.DateInitialize(s);
+            int year0 = DateTime.getYear();
+            int month0 = DateTime.getMonthValue();
+            int day0 = DateTime.getDayOfMonth();
+            // Scanner 로 받은 날짜 입력값과 동일하다면
+            if (ScrDate[0] == year0 && ScrDate[1] == month0 && ScrDate[2] == day0) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+}
--- src/Analysis/TimeFormat.java
+++ src/Analysis/TimeFormat.java
@@ -8,7 +8,6 @@ import java.time.ZonedDateTime;
 import java.util.Date;
 
 public class TimeFormat {
-
     public static ZonedDateTime DateInitialize(String DateString) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy'년 'MM'월 'dd'일 'a KK':'mm");
         Date date = new Date();
@@ -20,10 +19,5 @@ public class TimeFormat {
         Instant instant = date.toInstant();
         ZonedDateTime dateTime = ZonedDateTime.ofInstant(instant, ZoneId.systemDefault());
         return dateTime;
-    }
-
-    public static void main(String[] args) {
-        String DateString = "2019년 4월 29일 오후 5:50";
-        System.out.println(DateInitialize(DateString));
     }
 }
