bf95d987d7adb69c3393d1654c1c5cacba931d4a
kodh49 <milleniumkodh49@gmail.com>
2019-11-14 21:32:20 +0900

    Rename

--- src/Member/MakeFile.java
+++ src/Member/MakeFile.java
@@ -6,12 +6,12 @@ public class MakeFile {
 	String path;
 	
 	void makeFile() {
-		File folder=new File(path);
+		File folder = new File(path);
 		if(folder.mkdirs()) {
-			System.out.println("성공");
+			System.out.println("directory confirmed");
 			System.out.println(folder.getName());
 		}
-		else System.out.println("이미 있습니다.");
+		else System.out.println("Duplicated directory");
 	}
 
 }
